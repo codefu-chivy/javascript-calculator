4e84f71ba90f22b0d49630ed9c241bbfa4b2ef43
Chival Trotman <chivy360@gmail.com>
2018-01-07 20:08:22 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+    entry: __dirname + "/src/js/index.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                loaders: "babel-loader",
+                query: {
+                    cacheDirectory: "babel-cache"
+                }
+            }
+        ]
+    }
+}
